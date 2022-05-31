8e35c7da7873f195839182dc926005a1912e6242
psison <sisonpatrick29@yahoo.com>
2022-05-31 15:46:05 -0500

    complete lab

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-const inputEl = document.getElementById('next-to-do')
+const inputEl = document.getElementById('todo-input')
 const buttonEl = document.getElementById('submit-button')
 const todoList = document.getElementById('todo-list')
 
+buttonEl.addEventListener('click', addTodo)
+
+function addTodo() {
+    const newTodo = document.createElement('li')
+    newTodo.textContent = inputEl.value
+    document.querySelector('ul').appendChild(newTodo)
+    inputEl.value = ''
+}
