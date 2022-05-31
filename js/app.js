65d15c0669b28559fed36dd438e6bb2c1fe22309
psison <sisonpatrick29@yahoo.com>
2022-05-31 15:30:31 -0500

    create cached element references

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-console.log('check')
+const inputEl = document.getElementById('next-to-do')
+const buttonEl = document.getElementById('submit-button')
+const todoList = document.getElementById('todo-list')
+
